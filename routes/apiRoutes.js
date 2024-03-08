b209b0435345270a389bc6b5db89a4606e93971a
Aakash Patel <aakashpatel6250@gmail.com>
2024-03-08 19:40:46 +0530

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const apicontroller = require('../controllers/apicontroller');
+const apicontroller = require('../controllers/apiController');
 const upload = require('../controllers/multer');
 
 
