8207c883143e165e18f84e94392348455120f8ea
Aakash Patel <aakashpatel6250@gmail.com>
2024-03-18 23:37:03 +0530

    some little update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,24 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+var menu = document.querySelector('#menuicon');
+menu.addEventListener('click', function() {
+    var menutl= gsap.timeline();
+    if(menu.className==='ri-menu-line'){
+        menutl.to('.ri-menu-line',{
+            opacity:0
+        })
+        menutl.to('.ri-menu-line',{
+            className:'ri-close-line',
+            opacity:1,
+            rotate:180
+        })
+    }
+    else if(menu.className==='ri-close-line'){
+        menutl.to('.ri-close-line',{
+            opacity:0,
+            rotate:0
+        })
+        menutl.to('.ri-close-line',{
+            opacity:1,
+            className:'ri-menu-line'
+        })
+    }
 });
