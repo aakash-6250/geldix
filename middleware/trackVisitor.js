9bfa337a65fa0014dfee5c5210ef2f7266f97afb
Aakash Patel <aakashpatel6250@gmail.com>
2024-08-03 16:39:59 +0530

    update

--- middleware/trackVisitor.js
+++ middleware/trackVisitor.js
@@ -2,15 +2,21 @@ const Visitor = require('../models/visitors');
 const axios = require('axios');
 
 module.exports = async (req, res, next) => {
-    const ip = req.ip;
-    const response = await axios(`http://ip-api.com/json/${ip}`);
-    const { lat, lon, country, city } = response.data;
-    const existingVisitor = await Visitor.findOne({ ip });
-    if(!existingVisitor){
-        const visitor = await new Visitor({ip, location: {lat, lng:lon, country, city}}).save();
-    }else{
-        existingVisitor.location = {lat, lon, country, city};
-        existingVisitor.visits++;
-        await existingVisitor.save();
+    try {
+        const ip = req.ip.split(':').pop();
+        console.log(ip);
+        const response = await axios(`http://ip-api.com/json/${ip}`);
+        console.log(response.data)
+        const { lat, lon, country, city } = response.data;
+        const existingVisitor = await Visitor.findOne({ ip });
+        if (!existingVisitor) {
+            const visitor = await new Visitor({ ip, location: { lat, lng: lon, country, city } }).save();
+        } else {
+            existingVisitor.location = { lat, lon, country, city };
+            existingVisitor.visits++;
+            await existingVisitor.save();
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
