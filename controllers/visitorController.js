a5ffa79ec6ce82abb4fd5cea9103b1079af3ac9e
Aakash Patel <aakashpatel6250@gmail.com>
2024-08-05 17:23:18 +0530

    update

--- controllers/visitorController.js
+++ controllers/visitorController.js
@@ -2,7 +2,9 @@ const Visitors = require('../models/visitors')
 
 module.exports = async (req, res) => {
     try {
-        const visitors = await Visitors.find()
+        const visitors = await Visitors.aggregate([{
+            $sort: { updatedAt: -1 }
+        }])
         res.json(visitors)
     } catch (error) {
         res.status(500).json({ message: error.message })
